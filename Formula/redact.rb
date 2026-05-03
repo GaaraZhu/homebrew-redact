b3f51e2b5fe8a9439ee6b4a57811a9a6b31319c4
Gary Zhu <sec.gary@gmail.com>
2026-05-03 19:36:17 +1200

    fix: updates sha256 value for v0.1.0

--- Formula/redact.rb
+++ Formula/redact.rb
@@ -2,7 +2,7 @@ class Redact < Formula
   desc "PII-filtering CLI that intercepts AI agent query commands and redacts sensitive data"
   homepage "https://github.com/GaaraZhu/redact"
   url "https://github.com/GaaraZhu/redact/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2d3dde27fa254b85748993e47b2e3108433b032afb049b4c0426e533123957c8"
+  sha256 "7c05cce330ef5c3ded6732a23810ad672dff7aa314c727cdb8e3d3601e2943da"
   license "MIT"
   head "https://github.com/GaaraZhu/redact.git", branch: "main"
 
